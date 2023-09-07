2e54479c18568ce317e3fd6f5939eed6242e3a46
Nikita <barbos.nik007@gmail.com>
2023-09-07 15:05:05 +0200

    Works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import Loader from './Loader';
+import LoadMoreBtn from './LoadMoreBtn';
+import getImages from './services/api';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const App = () => {
+  const [serchReqest, setSerchReqest] = useState('');
+  const [status, setStatus] = useState('idle');
+  const [imagesList, setImagesList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [totalHits, setTotalHits] = useState(null);
+
+  useEffect(() => {
+    if (!serchReqest) {
+      return;
+    }
+    setStatus('pending');
+    getImages(serchReqest, page)
+      .then(images => {
+        if (images.hits.length === 0) {
+          setStatus('empty');
+          return;
+        }
+        setImagesList(prevImages => [...prevImages, ...images.hits]);
+        setStatus('completed');
+        setTotalHits(images.totalHits);
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('rejected');
+        console.log('у вас Ошибка => ', error);
+      });
+  }, [serchReqest, page]);
+
+  const onSubmit = req => {
+    if (req === serchReqest) {
+      return toast.error('Enter new request ^_^');
+    }
+    setSerchReqest(req);
+    setPage(1);
+    setImagesList([]);
+  };
+
+  const loadMoreBtnHandler = () => {
+    setPage(prState => prState + 1);
+  };
+
+  if (totalHits === imagesList.length) {
+    toast.error('Sorry, there are no more photos :(');
+  }
+  if (status === 'error') {
+    toast.error(`${error}`);
+  }
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app">
+      <Searchbar onSubmit={onSubmit} />
+      {status === 'idle' && (
+        <h1 className="temporaty-heading">Enter your request ⬆️</h1>
+      )}
+
+      {status === 'empty' && (
+        <h1 className="temporaty-heading">
+          No results by request "{serchReqest}" 😢
+        </h1>
+      )}
+
+      <ImageGallery images={imagesList} />
+
+      {status === 'pending' && <Loader />}
+
+      {imagesList.length !== 0 &&
+        totalHits !== imagesList.length &&
+        status !== 'pending' && (
+          <LoadMoreBtn onClickHandler={loadMoreBtnHandler} />
+        )}
+
+      <ToastContainer theme="colored" />
     </div>
   );
 };
